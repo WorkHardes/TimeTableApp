6e24dec2a633d966218f0e89cd2b9204e0233c6e
Arkady <arckadyor34@gmail.com>
2022-01-13 21:00:11 +0300

    nothing

---
+++ TestTimeTableOfClasses/UnitTest1.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+using TimeTableOfClasses.Domain;
+
+namespace TestTimeTableOfClasses
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
