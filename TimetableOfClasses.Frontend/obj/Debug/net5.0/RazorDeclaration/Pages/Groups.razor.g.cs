3c5090e7b713a4690d14d498c38b83c5ff3f7f67
Arkady <arckadyor34@gmail.com>
2022-01-14 01:57:07 +0300

    Arrgh

--- TimetableOfClasses.Frontend/obj/Debug/net5.0/RazorDeclaration/Pages/Groups.razor.g.cs
+++ TimetableOfClasses.Frontend/obj/Debug/net5.0/RazorDeclaration/Pages/Groups.razor.g.cs
@@ -13,70 +13,70 @@ namespace TimetableOfClasses.Frontend.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/Users/yaroslav/Desktop/LabaDotNet/TimeTableApp/TimetableOfClasses.Frontend/_Imports.razor"
+#line 1 "C:\CSharp\TimetableOfClasses\TimetableOfClasses.Frontend\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/yaroslav/Desktop/LabaDotNet/TimeTableApp/TimetableOfClasses.Frontend/_Imports.razor"
+#line 2 "C:\CSharp\TimetableOfClasses\TimetableOfClasses.Frontend\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/yaroslav/Desktop/LabaDotNet/TimeTableApp/TimetableOfClasses.Frontend/_Imports.razor"
+#line 3 "C:\CSharp\TimetableOfClasses\TimetableOfClasses.Frontend\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/Users/yaroslav/Desktop/LabaDotNet/TimeTableApp/TimetableOfClasses.Frontend/_Imports.razor"
+#line 4 "C:\CSharp\TimetableOfClasses\TimetableOfClasses.Frontend\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/Users/yaroslav/Desktop/LabaDotNet/TimeTableApp/TimetableOfClasses.Frontend/_Imports.razor"
+#line 5 "C:\CSharp\TimetableOfClasses\TimetableOfClasses.Frontend\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/Users/yaroslav/Desktop/LabaDotNet/TimeTableApp/TimetableOfClasses.Frontend/_Imports.razor"
+#line 6 "C:\CSharp\TimetableOfClasses\TimetableOfClasses.Frontend\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/Users/yaroslav/Desktop/LabaDotNet/TimeTableApp/TimetableOfClasses.Frontend/_Imports.razor"
+#line 7 "C:\CSharp\TimetableOfClasses\TimetableOfClasses.Frontend\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/Users/yaroslav/Desktop/LabaDotNet/TimeTableApp/TimetableOfClasses.Frontend/_Imports.razor"
+#line 8 "C:\CSharp\TimetableOfClasses\TimetableOfClasses.Frontend\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/Users/yaroslav/Desktop/LabaDotNet/TimeTableApp/TimetableOfClasses.Frontend/_Imports.razor"
+#line 9 "C:\CSharp\TimetableOfClasses\TimetableOfClasses.Frontend\_Imports.razor"
 using TimetableOfClasses.Frontend;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/Users/yaroslav/Desktop/LabaDotNet/TimeTableApp/TimetableOfClasses.Frontend/_Imports.razor"
+#line 10 "C:\CSharp\TimetableOfClasses\TimetableOfClasses.Frontend\_Imports.razor"
 using TimetableOfClasses.Frontend.Shared;
 
 #line default
@@ -91,7 +91,7 @@ using TimetableOfClasses.Frontend.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 37 "/Users/yaroslav/Desktop/LabaDotNet/TimeTableApp/TimetableOfClasses.Frontend/Pages/Groups.razor"
+#line 37 "C:\CSharp\TimetableOfClasses\TimetableOfClasses.Frontend\Pages\Groups.razor"
            
 private GroupType[] data;
 
